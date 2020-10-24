95917c131ac8fb31a9634fb931e80bb6a140ae52
Evgenii Chaikin <chaikin@strw.leidenuniv.nl>
2020-10-24 16:36:47 +0200

    Add 2nd axis (redshifts) to 1st axis (cosmic time)

--- colibre/scripts/simulation_time_number_of_steps.py
+++ colibre/scripts/simulation_time_number_of_steps.py
@@ -11,6 +11,9 @@ from swiftsimio import load
 from swiftpipeline.argumentparser import ScriptArgumentParser
 from glob import glob
 
+# Import EAGLE cosmology object
+from astropy.cosmology import Planck13
+
 arguments = ScriptArgumentParser(
     description="Creates a run performance plot: number of steps versus simulation time"
 )
@@ -23,6 +26,9 @@ output_path = arguments.output_directory
 plt.style.use(arguments.stylesheet_location)
 
 fig, ax = plt.subplots()
+
+# We will need to keep track of the maximum cosmic time reached in the simulation(s)
+t_max = unyt.unyt_array(0., units="Gyr")
 
 for run_name, run_directory, snapshot_name in zip(
     run_names, run_directories, snapshot_names
@@ -38,6 +44,11 @@ for run_name, run_directory, snapshot_name in zip(
     ).T
 
     sim_time = unyt.unyt_array(data[1], units=snapshot.units.time).to("Gyr")
+
+    # Update the maximum cosmic time if needed
+    if sim_time[-1] > t_max:
+        t_max = sim_time[-1]
+
     number_of_steps = np.arange(sim_time.size) / 1e6
 
     # Simulation data plotting
@@ -50,13 +61,29 @@ for run_name, run_directory, snapshot_name in zip(
         marker=".",
         zorder=10,
     )
+# Create second X-axis (to plot redshift alongside the cosmic time)
+ax2 = ax.twiny()
 
-ax.set_xlim(0, None)
+# z ticks along the second X-axis we want to display
+z_ticks = np.array([0, 0.2, 0.5, 1, 1.5, 2, 3, 10])
+
+# To place the new ticks onto the X-axis we need to know the corresponding cosmic times
+t_ticks = Planck13.age(z_ticks).value
+
+# Attach the ticks to the second X-axis
+ax2.set_xticks(t_ticks)
+
+# Format the ticks' labels
+ax2.set_xticklabels(["%2.1f" % z_tick for z_tick in z_ticks])
+
+ax.set_xlim(0, t_max * 1.05)
+ax2.set_xlim(0, t_max * 1.05)
 ax.set_ylim(0, None)
 
 ax.legend(loc="lower right")
 
 ax.set_ylabel("Number of steps [millions]")
 ax.set_xlabel("Simulation time [Gyr]")
+ax2.set_xlabel("Redshift z")
 
 fig.savefig(f"{output_path}/simulation_time_number_of_steps.png")
--- colibre/scripts/wallclock_simulation_time.py
+++ colibre/scripts/wallclock_simulation_time.py
@@ -11,6 +11,9 @@ from swiftsimio import load
 from swiftpipeline.argumentparser import ScriptArgumentParser
 from glob import glob
 
+# Import EAGLE cosmology object
+from astropy.cosmology import Planck13
+
 arguments = ScriptArgumentParser(
     description="Creates a run performance plot: simulation time versus wall-clock time"
 )
@@ -23,6 +26,9 @@ output_path = arguments.output_directory
 plt.style.use(arguments.stylesheet_location)
 
 fig, ax = plt.subplots()
+
+# We will need to keep track of the maximum cosmic time reached in the simulation(s)
+t_max = unyt.unyt_array(0., units="Gyr")
 
 for run_name, run_directory, snapshot_name in zip(
     run_names, run_directories, snapshot_names
@@ -38,6 +44,11 @@ for run_name, run_directory, snapshot_name in zip(
     ).T
 
     sim_time = unyt.unyt_array(data[1], units=snapshot.units.time).to("Gyr")
+
+    # Update the maximum cosmic time if needed
+    if sim_time[-1] > t_max:
+        t_max = sim_time[-1]
+
     wallclock_time = unyt.unyt_array(np.cumsum(data[-2]), units="ms").to("Hour")
 
     # Simulation data plotting
@@ -51,12 +62,29 @@ for run_name, run_directory, snapshot_name in zip(
         zorder=10,
     )
 
-ax.set_xlim(0, None)
+# Create second Y-axis (to plot redshift alongside the cosmic time)
+ax2 = ax.twinx()
+
+# z ticks along the second Y-axis we want to display
+z_ticks = np.array([0, 0.2, 0.5, 1, 1.5, 2, 3,  5, 10])
+
+# To place the new ticks onto the Y-axis we need to know the corresponding cosmic times
+t_ticks = Planck13.age(z_ticks).value
+
+# Attach the ticks to the second Y-axis
+ax2.set_yticks(t_ticks)
+
+# Format the ticks' labels
+ax2.set_yticklabels(["%2.1f" % z_tick for z_tick in z_ticks])
+
+ax.set_ylim(0, t_max * 1.05)
+ax2.set_ylim(0, t_max * 1.05)
 ax.set_xlim(0, None)
 
 ax.legend(loc="lower right")
 
 ax.set_ylabel("Simulation time [Gyr]")
+ax2.set_ylabel("Redshift z")
 ax.set_xlabel("Wallclock time [Hours]")
 
 fig.savefig(f"{output_path}/wallclock_simulation_time.png")

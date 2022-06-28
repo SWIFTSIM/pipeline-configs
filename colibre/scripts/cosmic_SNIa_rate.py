6254d0e250890a56246966da86eaaeabab241388
Bert Vandenbroucke <vandenbroucke@strw.leidenuniv.nl>
2022-06-28 14:12:51 +0200

    Added observational data to SNIa rate script. Added script to COLIBRE config.

--- colibre/scripts/cosmic_SNIa_rate.py
+++ colibre/scripts/cosmic_SNIa_rate.py
@@ -5,17 +5,14 @@ import unyt
 
 import matplotlib.pyplot as plt
 import numpy as np
+import glob
 
 from swiftsimio import load
-
-from load_sfh_data import read_obs_data
 
 from velociraptor.observations import load_observations
 
 from astropy.cosmology import z_at_value
 from astropy.units import Gyr
-
-SNIa_rate_output_units = 1.0 / (unyt.yr * unyt.Mpc ** 3)
 
 from swiftpipeline.argumentparser import ScriptArgumentParser
 
@@ -40,7 +37,11 @@ simulation_labels = []
 
 fig, ax = plt.subplots()
 
-ax.loglog()
+ax.semilogx()
+
+log_multiplicative_factor = 4
+multiplicative_factor = 10 ** log_multiplicative_factor
+SNIa_rate_output_units = 1.0 / (unyt.yr * unyt.Mpc ** 3)
 
 for idx, (snapshot_filename, SNIa_filename, name) in enumerate(
     zip(snapshot_filenames, SNIa_filenames, names)
@@ -65,12 +66,36 @@ for idx, (snapshot_filename, SNIa_filename, name) in enumerate(
     SNIa_rate = (data["SNIa rate"] * SNIa_rate_units).to(SNIa_rate_output_units)
 
     # High z-order as we always want these to be on top of the observations
-    simulation_lines.append(ax.plot(scale_factor, SNIa_rate.value, zorder=10000)[0])
+    simulation_lines.append(
+        ax.plot(scale_factor, SNIa_rate.value * multiplicative_factor, zorder=10000)[0]
+    )
     simulation_labels.append(name)
 
 
 observation_lines = []
 observation_labels = []
+
+path_to_obs_data = f"{arguments.config.config_directory}/{arguments.config.observational_data_directory}"
+observational_data = load_observations(
+    sorted(glob.glob(f"{path_to_obs_data}/data/CosmicSNIaRate/*.hdf5"))
+)
+
+for obs_data in observational_data:
+    observation_lines.append(
+        ax.errorbar(
+            obs_data.x.value,
+            obs_data.y.value * multiplicative_factor,
+            yerr=obs_data.y_scatter.value * multiplicative_factor,
+            label=obs_data.citation,
+            linestyle="none",
+            marker="o",
+            elinewidth=0.5,
+            markeredgecolor="none",
+            markersize=2,
+            zorder=-10,
+        )
+    )
+    observation_labels.append(f"{obs_data.citation}")
 
 redshift_ticks = np.array([0.0, 0.2, 0.5, 1.0, 2.0, 3.0, 5.0, 10.0, 20.0, 50.0, 100.0])
 redshift_labels = [
@@ -92,7 +117,11 @@ ax.set_xticks(a_ticks)
 ax.set_xticklabels(redshift_labels)
 
 observation_legend = ax.legend(
-    observation_lines, observation_labels, markerfirst=True, loc=3, fontsize=4, ncol=2
+    observation_lines,
+    observation_labels,
+    markerfirst=True,
+    loc="center right",
+    fontsize=4,
 )
 
 simulation_legend = ax.legend(
@@ -123,12 +152,14 @@ ax2.set_xticklabels(["$%2.1f$" % t_tick for t_tick in t_ticks])
 ax.tick_params(axis="x", which="minor", bottom=False)
 ax2.tick_params(axis="x", which="minor", top=False)
 
-ax.set_ylim(1.0e-6, 1.0e-4)
+ax.set_ylim(0.0, 1.5)
 ax.set_xlim(1.02, 0.07)
 ax2.set_xlim(1.02, 0.07)
 
 ax.set_xlabel("Redshift $z$")
-ax.set_ylabel(r"SNIa rate [yr$^{-1}$ Mpc$^{-3}$]")
+ax.set_ylabel(
+    f"SNIa rate [$10^{{-{log_multiplicative_factor}}}$ yr$^{{-1}}$ cMpc$^{{-3}}$]"
+)
 ax2.set_xlabel("Cosmic time [Gyr]")
 
 fig.savefig(f"{output_path}/SNIa_rate_history.png")

Correct units, use scipy.stats

flamingo/scripts/bh_accretion_modes.py
@@ -7,13 +7,10 @@ from swiftsimio import load
 from unyt import mh, cm, Gyr
 from matplotlib.colors import LogNorm
 from matplotlib.animation import FuncAnimation
-import glob
-from velociraptor.observations import load_observation
-from pylab import rcParams
 
 # Set the limits of the figure.
 mass_bounds = [1e5, 3e10]
-value_bounds = [0, 1.02]
+value_bounds = [-0.02, 1.02]
 bins = 25
 def_value = -1.0
 
@@ -23,9 +20,9 @@ def get_data(filename):
     data = load(filename)
 
     masses = data.black_holes.subgrid_masses.to("Msun")
-    values = np.absolute(data.black_holes.spins.value)
+    accretion_modes = data.black_holes.accretion_modes
 
-    return masses.value, values
+    return masses, accretion_modes
 
 
 def calculate_medians(filename, mass_bounds, value_bounds, bins):
@@ -48,8 +45,8 @@ def calculate_medians(filename, mass_bounds, value_bounds, bins):
     values_rest = values[masses < threshold_mass]
     masses_rest = masses[masses < threshold_mass]
 
-    medians, _, _ = stats.binned_statistic(
-        masses_rest, values_rest, statistic="median", bins=mass_bins
+    means, _, _ = stats.binned_statistic(
+        masses_rest, values_rest, statistic="mean", bins=mass_bins
     )
     percentile_10s, _, _ = stats.binned_statistic(
         masses_rest,
@@ -64,7 +61,7 @@ def calculate_medians(filename, mass_bounds, value_bounds, bins):
         bins=mass_bins,
     )
     mass_bins = np.array(
-        [(mass_bins[i] + mass_bins[i + 1]) / 2 for i in range(np.size(medians))]
+        [(mass_bins[i] + mass_bins[i + 1]) / 2 for i in range(np.size(means))]
     )
 
     return (
@@ -73,65 +70,36 @@ def calculate_medians(filename, mass_bounds, value_bounds, bins):
         masses_rest,
         values_rest,
         mass_bins,
-        medians,
+        means,
         percentile_10s,
         percentile_90s,
     )
 
 
 def make_single_image(
-    filenames,
-    names,
-    mass_bounds,
-    value_bounds,
-    number_of_simulations,
-    output_path,
-    observational_data,
+    filenames, names, mass_bounds, value_bounds, number_of_simulations, output_path
 ):
 
     fig, ax = plt.subplots()
 
     ax.set_xlabel("Black Hole Subgrid Masses $M_{\\rm sub}$ [M$_\odot$]")
-    ax.set_ylabel(r"Black Hole Spins (magnitude) $\vert a\vert$")
+    ax.set_ylabel(r"Fraction In Thin Disk Mode")
     ax.set_xscale("log")
 
     for filename, name in zip(filenames, names):
-        masses_most_massive, values_most_massive, masses_rest, values_rest, mass_bins, medians, percentile_10s, percentile_90s = calculate_medians(
+        masses_most_massive, values_most_massive, masses_rest, values_rest, mass_bins, means, percentile_10s, percentile_90s = calculate_medians(
             filename, mass_bounds, value_bounds, bins
         )
-        mask = medians > 0
-        fill_plot, = ax.plot(mass_bins, medians, label=name)
-        ax.fill_between(
-            mass_bins,
-            percentile_10s,
-            percentile_90s,
-            alpha=0.2,
-            facecolor=fill_plot.get_color(),
-        )
+        fill_plot, = ax.plot(mass_bins, means, label=name)
         scatter_plot = ax.scatter(
             masses_most_massive, values_most_massive, facecolor=fill_plot.get_color()
         )
-        if number_of_simulations == 1:
-            ax.scatter(
-                masses_rest,
-                values_rest,
-                s=0.75,
-                edgecolors="none",
-                marker="o",
-                alpha=0.5,
-                facecolor=fill_plot.get_color(),
-            )
-
-    rcParams.update({"lines.markersize": 5})
-    for index, observation in enumerate(observational_data):
-        obs = load_observation(observation)
-        obs.plot_on_axes(ax)
 
     ax.legend()
     ax.set_xlim(*mass_bounds)
     ax.set_ylim(*value_bounds)
 
-    fig.savefig(f"{output_path}/black_hole_spins.png")
+    fig.savefig(f"{output_path}/black_hole_accretion_modes.png")
 
     return
 
@@ -139,7 +107,7 @@ def make_single_image(
 if __name__ == "__main__":
     from swiftpipeline.argumentparser import ScriptArgumentParser
 
-    arguments = ScriptArgumentParser(description="Spin - BH mass relation")
+    arguments = ScriptArgumentParser(description="Accretion Mode - BH mass relation")
 
     snapshot_filenames = [
         f"{directory}/{snapshot}"
@@ -150,10 +118,6 @@ if __name__ == "__main__":
 
     plt.style.use(arguments.stylesheet_location)
 
-    obs_data = glob.glob(
-        f"{arguments.config.config_directory}/{arguments.config.observational_data_directory}/data/BlackHoleSpinBlackHoleMass/*.hdf5"
-    )
-
     make_single_image(
         filenames=snapshot_filenames,
         names=arguments.name_list,
@@ -161,5 +125,4 @@ if __name__ == "__main__":
         value_bounds=value_bounds,
         number_of_simulations=arguments.number_of_inputs,
         output_path=arguments.output_directory,
-        observational_data=obs_data,
     )

flamingo/scripts/bh_eddington_fractions.py
@@ -7,13 +7,10 @@ from swiftsimio import load
 from unyt import mh, cm, Gyr
 from matplotlib.colors import LogNorm
 from matplotlib.animation import FuncAnimation
-import glob
-from velociraptor.observations import load_observation
-from pylab import rcParams
 
 # Set the limits of the figure.
 mass_bounds = [1e5, 3e10]
-value_bounds = [0, 1.02]
+value_bounds = [1e-7, 1e1]
 bins = 25
 def_value = -1.0
 
@@ -23,9 +20,9 @@ def get_data(filename):
     data = load(filename)
 
     masses = data.black_holes.subgrid_masses.to("Msun")
-    values = np.absolute(data.black_holes.spins.value)
+    values = data.black_holes.eddington_fractions
 
-    return masses.value, values
+    return masses, values
 
 
 def calculate_medians(filename, mass_bounds, value_bounds, bins):
@@ -80,26 +77,22 @@ def calculate_medians(filename, mass_bounds, value_bounds, bins):
 
 
 def make_single_image(
-    filenames,
-    names,
-    mass_bounds,
-    value_bounds,
-    number_of_simulations,
-    output_path,
-    observational_data,
+    filenames, names, mass_bounds, value_bounds, number_of_simulations, output_path
 ):
 
     fig, ax = plt.subplots()
 
     ax.set_xlabel("Black Hole Subgrid Masses $M_{\\rm sub}$ [M$_\odot$]")
-    ax.set_ylabel(r"Black Hole Spins (magnitude) $\vert a\vert$")
+    ax.set_ylabel(
+        "Black Hole Eddington Fraction $\dot{m}=\dot{M}_{\\rm BH}/\dot{M}_{\\rm Edd}$"
+    )
     ax.set_xscale("log")
+    ax.set_yscale("log")
 
     for filename, name in zip(filenames, names):
         masses_most_massive, values_most_massive, masses_rest, values_rest, mass_bins, medians, percentile_10s, percentile_90s = calculate_medians(
             filename, mass_bounds, value_bounds, bins
         )
-        mask = medians > 0
         fill_plot, = ax.plot(mass_bins, medians, label=name)
         ax.fill_between(
             mass_bins,
@@ -121,17 +114,20 @@ def make_single_image(
                 alpha=0.5,
                 facecolor=fill_plot.get_color(),
             )
-
-    rcParams.update({"lines.markersize": 5})
-    for index, observation in enumerate(observational_data):
-        obs = load_observation(observation)
-        obs.plot_on_axes(ax)
+        ax.plot(
+            [1e-10, 1e11],
+            [0.01, 0.01],
+            color="black",
+            linestyle=":",
+            linewidth=0.75,
+            label="$\dot{m}=0.01$",
+        )
 
     ax.legend()
     ax.set_xlim(*mass_bounds)
     ax.set_ylim(*value_bounds)
 
-    fig.savefig(f"{output_path}/black_hole_spins.png")
+    fig.savefig(f"{output_path}/black_hole_eddington_fractions.png")
 
     return
 
@@ -139,7 +135,9 @@ def make_single_image(
 if __name__ == "__main__":
     from swiftpipeline.argumentparser import ScriptArgumentParser
 
-    arguments = ScriptArgumentParser(description="Spin - BH mass relation")
+    arguments = ScriptArgumentParser(
+        description="Eddington fraction - BH mass relation"
+    )
 
     snapshot_filenames = [
         f"{directory}/{snapshot}"
@@ -150,10 +148,6 @@ if __name__ == "__main__":
 
     plt.style.use(arguments.stylesheet_location)
 
-    obs_data = glob.glob(
-        f"{arguments.config.config_directory}/{arguments.config.observational_data_directory}/data/BlackHoleSpinBlackHoleMass/*.hdf5"
-    )
-
     make_single_image(
         filenames=snapshot_filenames,
         names=arguments.name_list,
@@ -161,5 +155,4 @@ if __name__ == "__main__":
         value_bounds=value_bounds,
         number_of_simulations=arguments.number_of_inputs,
         output_path=arguments.output_directory,
-        observational_data=obs_data,
     )

flamingo/scripts/bh_mass_function.py
@@ -11,69 +11,85 @@ from velociraptor.observations import load_observation
 from pylab import rcParams
 
 # Set the limits of the figure.
-x_bounds = [1e5,3e10] 
-value_bounds = [1e-8,1e-1]
-bins=20
-def_value = -1.
+x_bounds = [1e5, 3e10]
+value_bounds = [1e-8, 1e-1]
+bins = 20
+def_value = -1.0
+
 
 def get_data(filename):
 
     data = load(filename)
 
-    masses = 1e10 * np.array(data.black_holes.subgrid_masses.value)
-    values = masses
-    print(np.amax(values))
-    
+    values = data.black_holes.subgrid_masses.to("Msun")
+
     return values
 
+
 def calculate_medians(filename, x_bounds, value_bounds, bins):
-  
+
     values = get_data(filename)
     values = np.log10(values)
-    
+
     snapshot = load(filename)
     boxsize = snapshot.metadata.boxsize.to("Mpc")
     box_volume = boxsize[0] * boxsize[1] * boxsize[2]
-    
-    x_bins = np.linspace(
-        np.log10(x_bounds[0]), np.log10(x_bounds[1]), bins
-    )
-    #print(x_bins)
+
+    x_bins = np.linspace(np.log10(x_bounds[0]), np.log10(x_bounds[1]), bins)
     bin_width = (np.log10(x_bounds[1]) - np.log10(x_bounds[0])) / bins
 
     mass_func = []
-    mass_func_errors=[]
+    mass_func_errors = []
     for x in x_bins:
-        print(values)
-        #print(values>(x-bin_width*0.5))
-        values_sliced = values[(values>(x-bin_width*0.5))&(values<(x+bin_width*0.5))]
-        
-        mass_func.append(np.size(values_sliced)/box_volume)
-        mass_func_errors.append(np.sqrt(np.size(values_sliced))/box_volume)
+        values_sliced = values[
+            (values > (x - bin_width * 0.5)) & (values < (x + bin_width * 0.5))
+        ]
 
-    return x_bins, np.log10(mass_func), np.log10(np.array(mass_func)+np.array(mass_func_errors))-np.log10(np.array(mass_func))
+        mass_func.append(np.size(values_sliced) / box_volume)
+        mass_func_errors.append(np.sqrt(np.size(values_sliced)) / box_volume)
+
+    return (
+        x_bins,
+        np.log10(mass_func),
+        np.log10(np.array(mass_func) + np.array(mass_func_errors))
+        - np.log10(np.array(mass_func)),
+    )
 
 
-def make_single_image(filenames, names, x_bounds, value_bounds, number_of_simulations, output_path, observational_data):
+def make_single_image(
+    filenames,
+    names,
+    x_bounds,
+    value_bounds,
+    number_of_simulations,
+    output_path,
+    observational_data,
+):
 
     fig, ax = plt.subplots()
 
     ax.set_xlabel("Black Hole Mass $\log_{10}M_{\\rm BH}$ $[{\\rm M}_\odot]$")
-    ax.set_ylabel("Black Hole Mass Function $\log_{10}\Phi$ [${\\rm Mpc}^{-3}{\\rm dex}^{-1}$]")
-    ax.set_xscale('log')
-    ax.set_yscale('log')
+    ax.set_ylabel(
+        "Black Hole Mass Function $\log_{10}\Phi$ [${\\rm Mpc}^{-3}{\\rm dex}^{-1}$]"
+    )
+    ax.set_xscale("log")
+    ax.set_yscale("log")
 
     for filename, name in zip(filenames, names):
-        x_bins, mass_function, mass_function_errors = calculate_medians(filename, x_bounds, value_bounds, bins)
-        print(mass_function)
-        mask = mass_function>-100
-        fill_plot, = ax.plot(10**x_bins[mask],10**mass_function[mask], label=name)
-        ax.fill_between(10**x_bins[mask], 10**(mass_function[mask]-mass_function_errors[mask]),10**(mass_function[mask]+mass_function_errors[mask]),alpha=0.2, facecolor=fill_plot.get_color())
-        #scatter_plot = ax.scatter(masses_most_massive, values_most_massive, facecolor=fill_plot.get_color())
-        #ax.scatter(masses_rest, values_rest, s = 1.5, edgecolors='none', marker='o', alpha=0.5, facecolor=fill_plot.get_color())
-        #ax.plot([1e-10,1e11],[0.01,0.01],color='black',linestyle=':',linewidth=0.75, label='$\dot{m}=0.01$')
-    
-    rcParams.update({"lines.markersize" : 5})
+        x_bins, mass_function, mass_function_errors = calculate_medians(
+            filename, x_bounds, value_bounds, bins
+        )
+        mask = mass_function > -100
+        fill_plot, = ax.plot(10 ** x_bins[mask], 10 ** mass_function[mask], label=name)
+        ax.fill_between(
+            10 ** x_bins[mask],
+            10 ** (mass_function[mask] - mass_function_errors[mask]),
+            10 ** (mass_function[mask] + mass_function_errors[mask]),
+            alpha=0.2,
+            facecolor=fill_plot.get_color(),
+        )
+
+    rcParams.update({"lines.markersize": 5})
     for index, observation in enumerate(observational_data):
         obs = load_observation(observation)
         obs.plot_on_axes(ax)
@@ -100,9 +116,9 @@ if __name__ == "__main__":
     ]
 
     plt.style.use(arguments.stylesheet_location)
-    
+
     obs_data = glob.glob(
-    f"{arguments.config.config_directory}/{arguments.config.observational_data_directory}/data/BlackHoleMassFunction/*.hdf5"
+        f"{arguments.config.config_directory}/{arguments.config.observational_data_directory}/data/BlackHoleMassFunction/*.hdf5"
     )
 
     make_single_image(
@@ -112,5 +128,5 @@ if __name__ == "__main__":
         value_bounds=value_bounds,
         number_of_simulations=arguments.number_of_inputs,
         output_path=arguments.output_directory,
-        observational_data=obs_data
+        observational_data=obs_data,
     )

flamingo/scripts/bh_radio_luminosity_functions.py
@@ -1,9 +1,11 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import scipy.stats as stats
 
 from swiftsimio import load
 
-from unyt import mh, cm, Gyr
+import unyt
+from unyt import mh, cm, Gyr, speed_of_light
 from matplotlib.colors import LogNorm
 from matplotlib.animation import FuncAnimation
 import glob
@@ -11,72 +13,96 @@ from velociraptor.observations import load_observation
 from pylab import rcParams
 
 # Set the limits of the figure.
-x_bounds = [1e20,1e27] 
-value_bounds = [3e-9,5e-3]
-bins=20
-def_value = -1.
+x_bounds = [1e20, 1e27]
+value_bounds = [3e-9, 5e-3]
+bins = 20
+def_value = -1.0
+
 
 def get_data(filename):
 
     data = load(filename)
 
-    jet_effs = np.array(data.black_holes.jet_efficiencies.value)
-    jet_powers = 6.44*10**23*9*10**16*10**4*np.array(data.black_holes.accretion_rates.value)*jet_effs
+    masses = data.black_holes.subgrid_masses.to("Msun")
+    jet_effs = data.black_holes.jet_efficiencies
+    accr_rates = data.black_holes.accretion_rates.convert_to_units(unyt.kg / unyt.s)
+    jet_powers = jet_effs * speed_of_light ** 2 * accr_rates
+
+    # Convert from kinetic jet power to radio luminosity using relation from Heckmann & Best (2014)
     f_cav = 4
-    radio_luminosities = 1e25 * (jet_powers / 7e43 / f_cav)**(1/0.68)
-    values = radio_luminosities
-    print(np.amax(values))
-    
+    radio_luminosities = 1e25 * (jet_powers / unyt.Watt / 7e35 / f_cav) ** (1 / 0.68)
+    values = radio_luminosities * unyt.Watt / unyt.Hertz
+
     return values
 
+
 def calculate_medians(filename, x_bounds, value_bounds, bins):
-  
+
     values = get_data(filename)
-    
+
     snapshot = load(filename)
     boxsize = snapshot.metadata.boxsize.to("Mpc")
     box_volume = boxsize[0] * boxsize[1] * boxsize[2]
     values = np.log10(values)
-    
-    x_bins = np.linspace(
-        np.log10(x_bounds[0]), np.log10(x_bounds[1]), bins
-    )
-    #print(x_bins)
+
+    x_bins = np.linspace(np.log10(x_bounds[0]), np.log10(x_bounds[1]), bins)
+
     bin_width = (np.log10(x_bounds[1]) - np.log10(x_bounds[0])) / bins
 
     lum_func = []
-    lum_func_errors=[]
+    lum_func_errors = []
     for x in x_bins:
-        #print(values)
-        #print(values>(x-bin_width*0.5))
-        values_sliced = values[(values>(x-bin_width*0.5))&(values<(x+bin_width*0.5))]
-        
+        values_sliced = values[
+            (values > (x - bin_width * 0.5)) & (values < (x + bin_width * 0.5))
+        ]
+
         lum_func.append(np.size(values_sliced) / box_volume)
         lum_func_errors.append(np.sqrt(np.size(values_sliced)) / box_volume)
 
-    return x_bins, np.log10(lum_func), np.log10(np.array(lum_func)+np.array(lum_func_errors))-np.log10(np.array(lum_func))
+    return (
+        x_bins,
+        np.log10(lum_func),
+        np.log10(np.array(lum_func) + np.array(lum_func_errors))
+        - np.log10(np.array(lum_func)),
+    )
 
 
-def make_single_image(filenames, names, x_bounds, value_bounds, number_of_simulations, output_path, observational_data):
+def make_single_image(
+    filenames,
+    names,
+    x_bounds,
+    value_bounds,
+    number_of_simulations,
+    output_path,
+    observational_data,
+):
 
     fig, ax = plt.subplots()
 
     ax.set_xlabel("AGN Radio Luminosity $\log_{10}P_{\\rm 1.4GHz}$ $[{\\rm WHz^{-1}}]$")
-    ax.set_ylabel("AGN Radio Lum. Func. $\log_{10}\Phi_{\\rm 1.4GHz}$ [${\\rm Mpc}^{-3}{\\rm dex}^{-1}$]")
-    ax.set_xscale('log')
-    ax.set_yscale('log')
+    ax.set_ylabel(
+        "AGN Radio Lum. Func. $\log_{10}\Phi_{\\rm 1.4GHz}$ [${\\rm Mpc}^{-3}{\\rm dex}^{-1}$]"
+    )
+    ax.set_xscale("log")
+    ax.set_yscale("log")
 
     for filename, name in zip(filenames, names):
-        x_bins, luminosity_function, luminosity_function_errors = calculate_medians(filename, x_bounds, value_bounds, bins)
-        print(luminosity_function)
-        mask = luminosity_function>-100
-        fill_plot, = ax.plot(10**x_bins[mask],10**luminosity_function[mask], label=name)
-        ax.fill_between(10**x_bins[mask], 10**(luminosity_function[mask]-luminosity_function_errors[mask]),10**(luminosity_function[mask]+luminosity_function_errors[mask]),alpha=0.2, facecolor=fill_plot.get_color())
-        #scatter_plot = ax.scatter(masses_most_massive, values_most_massive, facecolor=fill_plot.get_color())
-        #ax.scatter(masses_rest, values_rest, s = 1.5, edgecolors='none', marker='o', alpha=0.5, facecolor=fill_plot.get_color())
-        #ax.plot([1e-10,1e11],[0.01,0.01],color='black',linestyle=':',linewidth=0.75, label='$\dot{m}=0.01$')
-    
-    rcParams.update({"lines.markersize" : 5})
+        x_bins, luminosity_function, luminosity_function_errors = calculate_medians(
+            filename, x_bounds, value_bounds, bins
+        )
+        mask = luminosity_function > -100
+        fill_plot, = ax.plot(
+            10 ** x_bins[mask], 10 ** luminosity_function[mask], label=name
+        )
+        ax.fill_between(
+            10 ** x_bins[mask],
+            10 ** (luminosity_function[mask] - luminosity_function_errors[mask]),
+            10 ** (luminosity_function[mask] + luminosity_function_errors[mask]),
+            alpha=0.2,
+            facecolor=fill_plot.get_color(),
+        )
+
+    rcParams.update({"lines.markersize": 5})
     for index, observation in enumerate(observational_data):
         obs = load_observation(observation)
         obs.plot_on_axes(ax)
@@ -103,9 +129,9 @@ if __name__ == "__main__":
     ]
 
     plt.style.use(arguments.stylesheet_location)
-    
+
     obs_data = glob.glob(
-    f"{arguments.config.config_directory}/{arguments.config.observational_data_directory}/data/BlackHoleAGNRadioLuminosityFunction/*.hdf5"
+        f"{arguments.config.config_directory}/{arguments.config.observational_data_directory}/data/BlackHoleAGNRadioLuminosityFunction/*.hdf5"
     )
 
     make_single_image(
@@ -115,5 +141,5 @@ if __name__ == "__main__":
         value_bounds=value_bounds,
         number_of_simulations=arguments.number_of_inputs,
         output_path=arguments.output_directory,
-        observational_data=obs_data
+        observational_data=obs_data,
     )

flamingo/scripts/bh_thermal_heatings.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import scipy.stats as stats
 
 from swiftsimio import load
 
@@ -8,78 +9,109 @@ from matplotlib.colors import LogNorm
 from matplotlib.animation import FuncAnimation
 
 # Set the limits of the figure.
-mass_bounds = [1e5,3e10] 
-value_bounds = [8e-1,1e5]
-bins=25
-def_value = -1.
+mass_bounds = [1e5, 3e10]
+value_bounds = [8e-1, 1e5]
+bins = 25
+def_value = -1.0
+
 
 def get_data(filename):
 
     data = load(filename)
 
     masses = data.black_holes.subgrid_masses.to("Msun")
-    values = np.array(data.black_holes.number_of_heating_events.value)
+    values = data.black_holes.number_of_heating_events
 
-    return np.array(masses.value)[values>0.0000000001], values[values>0.0000000001]
+    return masses, values
+
 
 def calculate_medians(filename, mass_bounds, value_bounds, bins):
-  
-    masses = get_data(filename)[0]
-    values = get_data(filename)[1]
-    
+
+    masses, values = get_data(filename)
+
     masses_10th_most_massive = np.sort(masses)[-7]
-    
-    mass_bins = np.logspace(
-        np.log10(mass_bounds[0]), np.log10(mass_bounds[1]), bins
-    )
+
+    mass_bins = np.logspace(np.log10(mass_bounds[0]), np.log10(mass_bounds[1]), bins)
     bin_width = (np.log10(mass_bounds[1]) - np.log10(mass_bounds[0])) / bins
 
-    threshold_mass = 10**(np.log10(mass_bins[mass_bins<masses_10th_most_massive][-1]) + bin_width * 0.5)
-    mass_bins = mass_bins[mass_bins<threshold_mass]
-    
-    
-    values_most_massive = values[masses>threshold_mass]
-    masses_most_massive = masses[masses>threshold_mass]
-    
-    values_rest = values[masses<threshold_mass]
-    masses_rest = masses[masses<threshold_mass]
-    
-    medians =[]
-    percentile_10s =[]
-    percentile_90s = []
-    for x in np.log10(mass_bins):
-        values_sliced = values_rest[(masses_rest>10**(x-bin_width*0.5))&(masses_rest<10**(x+bin_width*0.5))]
-        
-        if np.size(values_sliced)>0:
-            medians.append(np.mean(values_sliced))
-            percentile_10s.append(np.percentile(values_sliced,10))
-            percentile_90s.append(np.percentile(values_sliced,90))
-        else:
-            medians.append(def_value)
-            percentile_10s.append(def_value)
-            percentile_90s.append(def_value)
+    threshold_mass = 10 ** (
+        np.log10(mass_bins[mass_bins < masses_10th_most_massive][-1]) + bin_width * 0.5
+    )
+    mass_bins = mass_bins[mass_bins < threshold_mass]
 
-    return masses_most_massive, values_most_massive, masses_rest, values_rest, mass_bins, np.array(medians), np.array(percentile_10s), np.array(percentile_90s)
+    values_most_massive = values[masses > threshold_mass]
+    masses_most_massive = masses[masses > threshold_mass]
+
+    values_rest = values[masses < threshold_mass]
+    masses_rest = masses[masses < threshold_mass]
+
+    medians, _, _ = stats.binned_statistic(
+        masses_rest, values_rest, statistic="median", bins=mass_bins
+    )
+    percentile_10s, _, _ = stats.binned_statistic(
+        masses_rest,
+        values_rest,
+        statistic=lambda x: np.percentile(x, 10.0),
+        bins=mass_bins,
+    )
+    percentile_90s, _, _ = stats.binned_statistic(
+        masses_rest,
+        values_rest,
+        statistic=lambda x: np.percentile(x, 90.0),
+        bins=mass_bins,
+    )
+    mass_bins = np.array(
+        [(mass_bins[i] + mass_bins[i + 1]) / 2 for i in range(np.size(medians))]
+    )
+
+    return (
+        masses_most_massive,
+        values_most_massive,
+        masses_rest,
+        values_rest,
+        mass_bins,
+        medians,
+        percentile_10s,
+        percentile_90s,
+    )
 
 
-def make_single_image(filenames, names, mass_bounds, value_bounds, number_of_simulations, output_path):
+def make_single_image(
+    filenames, names, mass_bounds, value_bounds, number_of_simulations, output_path
+):
 
     fig, ax = plt.subplots()
 
     ax.set_xlabel("Black Hole Subgrid Masses $M_{\\rm sub}$ [M$_\odot$]")
     ax.set_ylabel("Number Of Particles Heated by AGN Feedback  $N_{\\rm th}$")
-    ax.set_xscale('log')
-    ax.set_yscale('log')
+    ax.set_xscale("log")
+    ax.set_yscale("log")
 
     for filename, name in zip(filenames, names):
-        masses_most_massive, values_most_massive, masses_rest, values_rest, mass_bins, medians, percentile_10s, percentile_90s = calculate_medians(filename, mass_bounds, value_bounds, bins)
-        mask = medians>0
-        fill_plot, = ax.plot(mass_bins[mask], medians[mask], label=name)
-        ax.fill_between(mass_bins[mask], percentile_10s[mask],percentile_90s[mask],alpha=0.2, facecolor=fill_plot.get_color())
-        scatter_plot = ax.scatter(masses_most_massive, values_most_massive, facecolor=fill_plot.get_color())
-        if number_of_simulations==1:
-            ax.scatter(masses_rest, values_rest, s = 0.75, edgecolors='none', marker='o', alpha=0.5, facecolor=fill_plot.get_color())
-        #ax.plot([1e-10,1e11],[0.01,0.01],color='black',linestyle=':',linewidth=0.75, label='$\dot{m}=0.01$')
+        masses_most_massive, values_most_massive, masses_rest, values_rest, mass_bins, medians, percentile_10s, percentile_90s = calculate_medians(
+            filename, mass_bounds, value_bounds, bins
+        )
+        fill_plot, = ax.plot(mass_bins, medians, label=name)
+        ax.fill_between(
+            mass_bins,
+            percentile_10s,
+            percentile_90s,
+            alpha=0.2,
+            facecolor=fill_plot.get_color(),
+        )
+        scatter_plot = ax.scatter(
+            masses_most_massive, values_most_massive, facecolor=fill_plot.get_color()
+        )
+        if number_of_simulations == 1:
+            ax.scatter(
+                masses_rest,
+                values_rest,
+                s=0.75,
+                edgecolors="none",
+                marker="o",
+                alpha=0.5,
+                facecolor=fill_plot.get_color(),
+            )
 
     ax.legend()
     ax.set_xlim(*mass_bounds)
@@ -93,7 +125,9 @@ def make_single_image(filenames, names, mass_bounds, value_bounds, number_of_sim
 if __name__ == "__main__":
     from swiftpipeline.argumentparser import ScriptArgumentParser
 
-    arguments = ScriptArgumentParser(description="Number of particles heated by AGN thermal feedback - BH mass relation")
+    arguments = ScriptArgumentParser(
+        description="Number of particles heated by AGN thermal feedback - BH mass relation"
+    )
 
     snapshot_filenames = [
         f"{directory}/{snapshot}"
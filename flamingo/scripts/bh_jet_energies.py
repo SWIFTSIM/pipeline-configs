Fix accretion rate units, introduce try/except, plot three most massive galaxies

flamingo/scripts/bh_jet_energies.py
@@ -20,11 +20,12 @@ def get_data(filename):
     data = load(filename)
 
     masses = data.black_holes.subgrid_masses.to("Msun")
-    values = data.black_holes.injected_jet_energies.astype(np.float64).to("erg")
 
-    if np.size(values) == 0:
+    try:
+        values = data.black_holes.injected_jet_energies.astype(np.float64).to("erg")
+    except:
         values = np.zeros(np.size(masses))
-
+        
     return masses, values
 
 
@@ -32,13 +33,13 @@ def calculate_medians(filename, mass_bounds, value_bounds, bins):
 
     masses, values = get_data(filename)
 
-    masses_10th_most_massive = np.sort(masses)[-7]
+    masses_3rd_most_massive = np.sort(masses)[-3]
 
     mass_bins = np.logspace(np.log10(mass_bounds[0]), np.log10(mass_bounds[1]), bins)
     bin_width = (np.log10(mass_bounds[1]) - np.log10(mass_bounds[0])) / bins
 
     threshold_mass = 10 ** (
-        np.log10(mass_bins[mass_bins < masses_10th_most_massive][-1]) + bin_width * 0.5
+        np.log10(mass_bins[mass_bins < masses_3rd_most_massive][-1]) + bin_width * 0.5
     )
     mass_bins = mass_bins[mass_bins < threshold_mass]
 

flamingo/scripts/bh_luminosities.py
@@ -22,25 +22,28 @@ def get_data(filename):
 
     masses = data.black_holes.subgrid_masses.to("Msun")
     rad_effs = data.black_holes.radiative_efficiencies
-    accr_rates = data.black_holes.accretion_rates.convert_to_units(unyt.kg / unyt.s)
-    values = rad_effs * speed_of_light ** 2 * accr_rates
-    values = values.convert_to_units(unyt.erg / unyt.s)
+    accr_rates = data.black_holes.accretion_rates.astype(np.float64).to(unyt.kg / unyt.s)
+    values = rad_effs * (speed_of_light ** 2) * accr_rates
+    values = values.to(unyt.erg / unyt.s)
 
     # Make sure to take only black holes with a non-zero luminosity
-    return masses[rad_effs > 1e-6], values[rad_effs > 1e-6]
+    masses = masses[rad_effs > 1e-6]
+    values = values[rad_effs > 1e-6]
+        
+    return masses, values
 
 
 def calculate_medians(filename, mass_bounds, value_bounds, bins):
 
     masses, values = get_data(filename)
 
-    masses_10th_most_massive = np.sort(masses)[-7]
+    masses_3rd_most_massive = np.sort(masses)[-3]
 
     mass_bins = np.logspace(np.log10(mass_bounds[0]), np.log10(mass_bounds[1]), bins)
     bin_width = (np.log10(mass_bounds[1]) - np.log10(mass_bounds[0])) / bins
 
     threshold_mass = 10 ** (
-        np.log10(mass_bins[mass_bins < masses_10th_most_massive][-1]) + bin_width * 0.5
+        np.log10(mass_bins[mass_bins < masses_3rd_most_massive][-1]) + bin_width * 0.5
     )
     mass_bins = mass_bins[mass_bins < threshold_mass]
 

flamingo/scripts/bh_radio_luminosity_functions.py
@@ -24,14 +24,18 @@ def get_data(filename):
     data = load(filename)
 
     masses = data.black_holes.subgrid_masses.to("Msun")
-    jet_effs = data.black_holes.jet_efficiencies
-    accr_rates = data.black_holes.accretion_rates.convert_to_units(unyt.kg / unyt.s)
-    jet_powers = jet_effs * speed_of_light ** 2 * accr_rates
+    
+    try:
+        jet_effs = data.black_holes.jet_efficiencies
+        accr_rates = data.black_holes.accretion_rates.to(unyt.kg / unyt.s)
+        jet_powers = jet_effs * speed_of_light ** 2 * accr_rates
 
-    # Convert from kinetic jet power to radio luminosity using relation from Heckmann & Best (2014)
-    f_cav = 4
-    radio_luminosities = 1e25 * (jet_powers / unyt.Watt / 7e35 / f_cav) ** (1 / 0.68)
-    values = radio_luminosities * unyt.Watt / unyt.Hertz
+        # Convert from kinetic jet power to radio luminosity using relation from Heckmann & Best (2014)
+        f_cav = 4
+        radio_luminosities = 1e25 * (jet_powers / unyt.Watt / 7e35 / f_cav) ** (1 / 0.68)
+        values = radio_luminosities * unyt.Watt / unyt.Hertz
+    except: 
+        values = np.zeros(np.size(masses))
 
     return values
 

flamingo/scripts/bh_spins_sign.py
@@ -20,8 +20,12 @@ def get_data(filename):
     data = load(filename)
 
     masses = data.black_holes.subgrid_masses.to("Msun")
-    values = np.sign(data.black_holes.spins.value)
-    values[values < 0] = 0
+    
+    try:
+        values = np.sign(data.black_holes.spins.value)
+        values[values < 0] = 0
+    except:
+        values = np.zeros(np.size(masses))
 
     return masses, values
 
@@ -30,13 +34,13 @@ def calculate_medians(filename, mass_bounds, value_bounds, bins):
 
     masses, values = get_data(filename)
 
-    masses_10th_most_massive = np.sort(masses)[-7]
+    masses_3rd_most_massive = np.sort(masses)[-3]
 
     mass_bins = np.logspace(np.log10(mass_bounds[0]), np.log10(mass_bounds[1]), bins)
     bin_width = (np.log10(mass_bounds[1]) - np.log10(mass_bounds[0])) / bins
 
     threshold_mass = 10 ** (
-        np.log10(mass_bins[mass_bins < masses_10th_most_massive][-1]) + bin_width * 0.5
+        np.log10(mass_bins[mass_bins < masses_3rd_most_massive][-1]) + bin_width * 0.5
     )
     mass_bins = mass_bins[mass_bins < threshold_mass]
 
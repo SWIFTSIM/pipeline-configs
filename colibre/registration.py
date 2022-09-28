938acc2d0419ef066e00c3a4132722f135cfa993
folkert <nobels@strw.leidenuniv.nl>
2022-09-28 18:06:13 +0100

    Add observational data

--- colibre/registration.py
+++ colibre/registration.py
@@ -94,7 +94,21 @@ def register_spesific_star_formation_rates(self, catalogue, aperture_sizes):
             (stellar_mass > unyt.unyt_quantity(1e10, units="Msun")).astype(float),
             units="dimensionless",
         )
-        is_active.name = "Stellar mass larger than 10^10 Msun"
+        is_bigger_than_1e10.name = "Stellar mass larger than 10^10 Msun"
+
+        # Mask for galaxies above 5* 10^10 Msun
+        is_bigger_than_5e10 = unyt.unyt_array(
+            (stellar_mass > unyt.unyt_quantity(5e10, units="Msun")).astype(float),
+            units="dimensionless",
+        )
+        is_bigger_than_5e10.name = "Stellar mass larger than 5 $\\times$ 10^10 Msun"
+
+        # Mask for galaxies above 5 * 10^10 Msun
+        is_bigger_than_5e10_active = unyt.unyt_array(
+            ((stellar_mass > unyt.unyt_quantity(5e10, units="Msun")) & (ssfr > 1.01 * marginal_ssfr)).astype(float),
+            units="dimensionless",
+        )
+        is_bigger_than_5e10_active.name = "Stellar mass larger than 5 $\\times$ 10^10 Msun and active"
 
         # Get the specific star formation rate (per halo mass instead of stellar mass)
         sfr_M200 = star_formation_rate / halo_mass
@@ -109,6 +123,16 @@ def register_spesific_star_formation_rates(self, catalogue, aperture_sizes):
             self,
             f"stellar_mass_is_bigger_than_1e10_msun_{aperture_size}_kpc",
             is_bigger_than_1e10,
+        )
+        setattr(
+            self,
+            f"stellar_mass_is_bigger_than_5e10_msun_{aperture_size}_kpc",
+            is_bigger_than_5e10,
+        )
+        setattr(
+            self,
+            f"stellar_mass_is_bigger_than_5e10_msun_active_{aperture_size}_kpc",
+            is_bigger_than_5e10_active,
         )
 
     return

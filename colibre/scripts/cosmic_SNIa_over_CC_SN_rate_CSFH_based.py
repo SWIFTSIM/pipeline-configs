7c1b1180b22869a9f226ed1352a0ee849c339c89
Folkert Nobels <Fonotec@users.noreply.github.com>
2023-05-22 09:59:09 +0200

    Update colibre/scripts/cosmic_SNIa_over_CC_SN_rate_CSFH_based.py
    Co-authored-by: Evgenii Chaikin <bowowoda@gmail.com>

--- colibre/scripts/cosmic_SNIa_over_CC_SN_rate_CSFH_based.py
+++ colibre/scripts/cosmic_SNIa_over_CC_SN_rate_CSFH_based.py
@@ -112,7 +112,7 @@ for idx, (snapshot_filename, sfr_filename, name) in enumerate(
         exponential_decay = float(exponential_decay) * unyt.Gyr
 
     Gaussian_SNIa_efficiency = snapshot.metadata.parameters.get("SNIaDTD:SNIa_efficiency_gauss_p_Msun", None)
-    if Gaussian_SNIa_efficiency == None:
+    if Gaussian_SNIa_efficiency is None:
         have_Gaussian = False
     else:
         have_Gaussian = True

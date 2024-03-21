c665dca2aeaa8b2f143bc0b7d46d69c0b442e79d
Filip Husko <filip.husko@durham.ac.uk>
2024-03-21 10:50:05 +0000

    Run formatting

--- colibre/scripts/bh_accretion_evolution.py
+++ colibre/scripts/bh_accretion_evolution.py
@@ -88,7 +88,7 @@ observation_lines.append(
     ax.plot(
         Aird2015.x.value,
         Aird2015.y.value,
-        color='brown',
+        color="brown",
         zorder=-10000,
         alpha=0.6,
         linewidth=1,
@@ -105,7 +105,7 @@ observation_lines.append(
         Annana2020_low.y.value,
         label=Annana2020_low.citation,
         zorder=-10000,
-        color='orange',
+        color="orange",
         alpha=0.6,
         linewidth=1,
     )[0]
@@ -120,7 +120,7 @@ observation_lines.append(
         Annana2020_high.x.value,
         Annana2020_high.y.value,
         label=Annana2020_high.citation,
-        color='olive',
+        color="olive",
         zorder=-10000,
         alpha=0.6,
         linewidth=1,
@@ -136,7 +136,7 @@ observation_lines.append(
         Pouliasis2024_low.x.value,
         Pouliasis2024_low.y.value - Pouliasis2024_low.y_scatter[0].value,
         Pouliasis2024_low.y.value + Pouliasis2024_low.y_scatter[1].value,
-        color='magenta',
+        color="magenta",
         label=Pouliasis2024_low.citation,
         zorder=-10000,
         alpha=0.2,
@@ -152,7 +152,7 @@ observation_lines.append(
         Pouliasis2024_high.x.value,
         Pouliasis2024_high.y.value - Pouliasis2024_high.y_scatter[0].value,
         Pouliasis2024_high.y.value + Pouliasis2024_high.y_scatter[1].value,
-        color='teal',
+        color="teal",
         label=Pouliasis2024_high.citation,
         zorder=-10000,
         alpha=0.2,
@@ -170,8 +170,12 @@ observation_lines.append(
     ax.errorbar(
         Delvacchio2014.x.value,
         Delvacchio2014.y.value,
-        xerr=None if Delvacchio2014.x_scatter is None else Delvacchio2014.x_scatter.value,
-        yerr=None if Delvacchio2014.y_scatter is None else Delvacchio2014.y_scatter.value,
+        xerr=None
+        if Delvacchio2014.x_scatter is None
+        else Delvacchio2014.x_scatter.value,
+        yerr=None
+        if Delvacchio2014.y_scatter is None
+        else Delvacchio2014.y_scatter.value,
         label=Delvacchio2014.citation,
         linestyle="none",
         marker="o",
@@ -235,14 +239,14 @@ observation_lines.append(
         Shen2020_low.y.value,
         xerr=None if Shen2020_low.x_scatter is None else Shen2020_low.x_scatter.value,
         yerr=None if Shen2020_low.y_scatter is None else Shen2020_low.y_scatter.value,
-            label=Shen2020_low.citation,
-            linestyle="none",
-            marker="o",
-            color="lime",
-            elinewidth=0.5,
-            markeredgecolor="none",
-            markersize=2,
-            zorder=-10,
+        label=Shen2020_low.citation,
+        linestyle="none",
+        marker="o",
+        color="lime",
+        elinewidth=0.5,
+        markeredgecolor="none",
+        markersize=2,
+        zorder=-10,
     )
 )
 observation_labels.append(Shen2020_low.citation)
@@ -256,14 +260,14 @@ observation_lines.append(
         Shen2020_high.y.value,
         xerr=None if Shen2020_high.x_scatter is None else Shen2020_high.x_scatter.value,
         yerr=None if Shen2020_high.y_scatter is None else Shen2020_high.y_scatter.value,
-            label=Shen2020_high.citation,
-            linestyle="none",
-            marker="o",
-            color="coral",
-            elinewidth=0.5,
-            markeredgecolor="none",
-            markersize=2,
-            zorder=-10,
+        label=Shen2020_high.citation,
+        linestyle="none",
+        marker="o",
+        color="coral",
+        elinewidth=0.5,
+        markeredgecolor="none",
+        markersize=2,
+        zorder=-10,
     )
 )
 observation_labels.append(Shen2020_high.citation)
@@ -294,9 +298,14 @@ ax.tick_params(axis="x", which="minor", bottom=False)
 ax.set_xlim(1.02, 0.07)
 ax.set_ylim(1e-7, 6e-4)
 
-#observation_legend = ax.legend(markerfirst=True, loc="lower left")
+# observation_legend = ax.legend(markerfirst=True, loc="lower left")
 observation_legend = ax.legend(
-    observation_lines, observation_labels, markerfirst=True, loc="lower center", fontsize=4, ncol=1
+    observation_lines,
+    observation_labels,
+    markerfirst=True,
+    loc="lower center",
+    fontsize=4,
+    ncol=1,
 )
 
 ax.add_artist(observation_legend)

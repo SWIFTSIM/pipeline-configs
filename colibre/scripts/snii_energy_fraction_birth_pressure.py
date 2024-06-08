49eee8a5df459d735ef42c6f60b87e36ddbeb6e0
Evgenii Chaikin <bowowoda@gmail.com>
2024-06-08 16:48:20 +0200

    Add two plots to the pipeline showing SN energy fractions (#283)
    - Add a plot that show the relation between the SN energy fraction and stlelar birth pressure
    - Add a plot that show the relation between the SN energy fraction and galaxy stellar mass
    - Rewrite the code in the script that computes the distribution of SN energy fractions, to make it more readable and maintainable

--- colibre/scripts/snii_energy_fraction_birth_pressure.py
+++ colibre/scripts/snii_energy_fraction_birth_pressure.py
@@ -1,15 +1,17 @@
 """
-Plots the SNII energy fraction distribution.
+Plots the SNII energy fraction vs. stellar birth pressure.
 """
 
 import matplotlib.pyplot as plt
 import numpy as np
 import unyt
 
-from unyt import mh
-
 from swiftsimio import load
 from swiftpipeline.argumentparser import ScriptArgumentParser
+
+# Set x-axis limits
+pressure_bounds = [1e1, 1e9]
+log_pressure_bounds = np.log10(pressure_bounds)
 
 
 def energy_fraction(
@@ -46,7 +48,7 @@ def get_snapshot_param_float(snapshot, param_name: str) -> float:
 
 
 arguments = ScriptArgumentParser(
-    description="Creates an SNII energy fraction distribution plot, split by redshift"
+    description="Creates a plot of SNII energy fraction vs. birth pressure"
 )
 
 
@@ -61,32 +63,19 @@ output_path = arguments.output_directory
 plt.style.use(arguments.stylesheet_location)
 
 data = [load(snapshot_filename) for snapshot_filename in snapshot_filenames]
+
 number_of_bins = 128
-
-energy_fraction_bins = unyt.unyt_array(
-    np.linspace(0, 5.0, number_of_bins), units="dimensionless"
+birth_pressures = unyt.unyt_array(
+    np.logspace(*log_pressure_bounds, number_of_bins), "K/cm**3"
 )
-energy_fraction_bin_width = (
-    energy_fraction_bins[1].value - energy_fraction_bins[0].value
-)
-energy_fraction_centers = 0.5 * (energy_fraction_bins[1:] + energy_fraction_bins[:-1])
-
 
 # Begin plotting
-fig, axes = plt.subplots(3, 1, sharex=True, sharey=True)
-axes = axes.flat
-
-ax_dict = {"$z < 1$": axes[0], "$1 < z < 3$": axes[1], "$z > 3$": axes[2]}
-
-for label, ax in ax_dict.items():
-    ax.text(0.025, 1.0 - 0.025 * 3, label, transform=ax.transAxes, ha="left", va="top")
-
+fig, ax = plt.subplots(1, 1)
 for color, (snapshot, name) in enumerate(zip(data, names)):
 
-    birth_densities = snapshot.stars.birth_densities.to("g/cm**3") / mh.to("g")
-    birth_temperatures = snapshot.stars.birth_temperatures.to("K")
-    birth_pressures = (birth_densities * birth_temperatures).to("K/cm**3")
-    birth_redshifts = 1 / snapshot.stars.birth_scale_factors.value - 1
+    # Default value that will be plotted outside the plot's domain
+    # if the true value is not found in the snapshot's metadata
+    pivot_pressure = 1e-10
 
     try:
         # Extract feedback parameters from snapshot metadata
@@ -118,48 +107,23 @@ for color, (snapshot, name) in enumerate(zip(data, names)):
             -np.ones_like(birth_pressures.value), "dimensionless"
         )
 
-    # Segment birth pressures into redshift bins
-    energy_fraction_by_redshift = {
-        "$z < 1$": energy_fractions[birth_redshifts < 1],
-        "$1 < z < 3$": energy_fractions[
-            np.logical_and(birth_redshifts > 1, birth_redshifts < 3)
-        ],
-        "$z > 3$": energy_fractions[birth_redshifts > 3],
-    }
+    z = snapshot.metadata.z
+    ax.plot(
+        birth_pressures.value,
+        energy_fractions,
+        label=f"{name} ($z={z:.1f})$",
+        color=f"C{color}",
+    )
 
-    # Total number of stars formed
-    Num_of_stars_total = len(birth_redshifts)
+    # Add the pivot pressure line
+    ax.axvline(
+        pivot_pressure, color=f"C{color}", linestyle="dashed", zorder=-10, alpha=0.5
+    )
 
-    # Average energy fraction (computed among all star particles)
-    average_energy_fraction = np.mean(energy_fractions)
+ax.set_xlim(*pressure_bounds)
+ax.set_xscale("log")
+ax.legend(loc="upper left", markerfirst=False)
+ax.set_ylabel("SNII Energy Fraction $f_{\\rm E}$")
+ax.set_xlabel("Stellar Birth Pressure $P_B/k$ [K cm$^{-3}$]")
 
-    for redshift, ax in ax_dict.items():
-        data = energy_fraction_by_redshift[redshift]
-
-        H, _ = np.histogram(data, bins=energy_fraction_bins)
-        y_points = H / energy_fraction_bin_width / Num_of_stars_total
-
-        ax.plot(energy_fraction_centers, y_points, label=name, color=f"C{color}")
-
-        # Add the median snii energy fraction
-        ax.axvline(
-            np.median(data),
-            color=f"C{color}",
-            linestyle="dashed",
-            zorder=-10,
-            alpha=0.5,
-        )
-
-        ax.axvline(
-            average_energy_fraction,
-            color=f"C{color}",
-            linestyle="dotted",
-            zorder=-10,
-            alpha=0.5,
-        )
-
-axes[0].legend(loc="upper right", markerfirst=False)
-axes[2].set_xlabel("SNII Energy Fraction $f_{\\rm E}$")
-axes[1].set_ylabel("$N_{\\rm bin}$ / d$f_{\\rm E}$ / $N_{\\rm total}$")
-
-fig.savefig(f"{arguments.output_directory}/snii_energy_fraction_distribution.png")
+fig.savefig(f"{arguments.output_directory}/snii_energy_fraction_birth_pressure.png")

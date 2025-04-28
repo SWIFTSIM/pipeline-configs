1de778b2194d55b4884d3f733a64b52db05c3deb
robjmcgibbon <robjmcgibbon@gmail.com>
2025-04-28 15:29:10 +0100

    Run pipeline on EAGLE

---
+++ run_pipeline.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+#SBATCH -J pipeline
+#SBATCH -o %x.%J.out
+##SBATCH -e %x.%J.err
+#SBATCH -p cosma8
+#SBATCH -A dp004
+#SBATCH -t 04:00:00
+
+export OPENBLAS_NUM_THREADS=2
+
+source /cosma/apps/dp004/dc-mcgi1/python_env/pipeline/bin/activate
+
+### L025m6
+swift-pipeline -C ./colibre \
+    -c SOAP_uncompressed/halo_properties_0028.hdf5 \
+    -s swift_snapshots/swift_028/snap_028.0.hdf5 \
+    -i "/snap7/scratch/dp004/dc-mcgi1/SOAP_EAGLE/L0025N0376" \
+    -o "/snap7/scratch/dp004/dc-mcgi1/SOAP_EAGLE/L0025N0376/pipeline"
+exit
+
+### L025m5
+
+swift-pipeline -C ./colibre \
+    -c SOAP/halo_properties_0127.hdf5 \
+    -s snapshots/colibre_0127/colibre_0127.hdf5 \
+    -i "/snap7/scratch/dp004/dc-mcgi1/SOAP_EAGLE/L0025N0752/Thermal_approx_grav" \
+    -o "/snap7/scratch/dp004/dc-mcgi1/SOAP_EAGLE/L0025N0752/Thermal_approx_grav/pipeline"
+
+swift-pipeline -C ./colibre \
+    -c SOAP_uncompressed/halo_properties_0028.hdf5 \
+    -s swift_snapshots/swift_028/snap_028.0.hdf5 \
+    -i "/snap7/scratch/dp004/dc-mcgi1/SOAP_EAGLE/L0025N0752" \
+    -o "/snap7/scratch/dp004/dc-mcgi1/SOAP_EAGLE/L0025N0752/pipeline"
+
+swift-pipeline -C ./colibre \
+    -c SOAP_uncompressed/halo_properties_0028.hdf5 SOAP/halo_properties_0127.hdf5 \
+    -n EAGLE COLIBRE_THERMAL \
+    -s swift_snapshots/swift_028/snap_028.0.hdf5 snapshots/colibre_0127/colibre_0127.hdf5 \
+    -i "/snap7/scratch/dp004/dc-mcgi1/SOAP_EAGLE/L0025N0752" "/snap7/scratch/dp004/dc-mcgi1/SOAP_EAGLE/L0025N0752/Thermal_approx_grav" \
+    -o "/snap7/scratch/dp004/dc-mcgi1/SOAP_EAGLE/L0025N0752/L25_comparison" \
+    -j 10
+
+#### L100m6
+
+swift-pipeline -C ./colibre \
+    -c SOAP/halo_properties_0127.hdf5 \
+    -s snapshots/colibre_0127/colibre_0127.hdf5 \
+    -i "/snap7/scratch/dp004/dc-mcgi1/SOAP_EAGLE/L0100N1504/Thermal" \
+    -o "/snap7/scratch/dp004/dc-mcgi1/SOAP_EAGLE/L0100N1504/Thermal/pipeline"
+
+swift-pipeline -C ./colibre \
+    -c SOAP_uncompressed/halo_properties_0028.hdf5 \
+    -s swift_snapshots/swift_028/snap_028.0.hdf5 \
+    -i "/snap7/scratch/dp004/dc-mcgi1/SOAP_EAGLE/L0100N1504" \
+    -o "/snap7/scratch/dp004/dc-mcgi1/SOAP_EAGLE/L0100N1504/pipeline"
+
+swift-pipeline -C ./colibre \
+    -c SOAP_uncompressed/halo_properties_0028.hdf5 SOAP/halo_properties_0127.hdf5 \
+    -n EAGLE COLIBRE_THERMAL \
+    -s swift_snapshots/swift_028/snap_028.0.hdf5 snapshots/colibre_0127/colibre_0127.hdf5 \
+    -i "/snap7/scratch/dp004/dc-mcgi1/SOAP_EAGLE/L0100N1504" "/snap7/scratch/dp004/dc-mcgi1/SOAP_EAGLE/L0100N1504/Thermal" \
+    -o "/snap7/scratch/dp004/dc-mcgi1/SOAP_EAGLE/L0100N1504/L100_comparison" \
+    -j 10
+
+echo "Job complete!"

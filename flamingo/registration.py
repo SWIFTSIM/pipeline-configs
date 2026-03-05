826fa093419aaed28a2863ea584e927de58f0e40
robjmcgibbon <robjmcgibbon@gmail.com>
2026-03-05 12:08:19 +0000

    Update flamingo config to work with SOAP catalogues

--- flamingo/registration.py
+++ flamingo/registration.py
@@ -33,13 +33,17 @@ aperture_sizes = [30, 50, 100]
 marginal_ssfr = unyt.unyt_quantity(1e-11, units=1 / unyt.year)
 
 for aperture_size in aperture_sizes:
-    halo_mass = catalogue.masses.mass_200crit
+    halo_mass = catalogue.get_quantity("masses.mass_200crit")
 
-    stellar_mass = getattr(catalogue.apertures, f"mass_star_{aperture_size}_kpc")
+    stellar_mass = catalogue.get_quantity(
+        f"apertures.mass_star_{aperture_size}_kpc"
+    )
     # Need to mask out zeros, otherwise we get RuntimeWarnings
     good_stellar_mass = stellar_mass > unyt.unyt_quantity(0.0, stellar_mass.units)
 
-    star_formation_rate = getattr(catalogue.apertures, f"sfr_gas_{aperture_size}_kpc")
+    star_formation_rate = catalogue.get_quantity(
+        f"apertures.sfr_gas_{aperture_size}_kpc"
+    )
 
     ssfr = np.ones(len(star_formation_rate)) * marginal_ssfr
     ssfr[good_stellar_mass] = (
@@ -78,9 +82,10 @@ twelve_plus_log_OH_solar = 8.69
 minimal_twelve_plus_log_OH = 7.5
 
 for aperture_size in aperture_sizes:
+
     try:
         metal_mass_fraction_star = (
-            getattr(catalogue.apertures, f"zmet_star_{aperture_size}_kpc")
+            catalogue.get_quantity(f"apertures.zmet_star_{aperture_size}_kpc")
             / solar_metal_mass_fraction
         )
         metal_mass_fraction_star.name = f"Star Metallicity $Z_*$ rel. to $Z_\\odot={solar_metal_mass_fraction}$ ({aperture_size} kpc)"
@@ -94,7 +99,7 @@ for aperture_size in aperture_sizes:
 
     try:
         metal_mass_fraction_gas = (
-            getattr(catalogue.apertures, f"zmet_gas_sf_{aperture_size}_kpc")
+            catalogue.get_quantity(f"apertures.zmet_gas_sf_{aperture_size}_kpc")
             / solar_metal_mass_fraction
         )
 
@@ -122,8 +127,10 @@ for aperture_size in aperture_sizes:
 # Now stellar mass - halo mass relation
 
 for aperture_size in aperture_sizes:
-    stellar_mass = getattr(catalogue.apertures, f"mass_star_{aperture_size}_kpc")
-    halo_mass = catalogue.masses.mass_200crit
+    stellar_mass = catalogue.get_quantity(
+        f"apertures.mass_star_{aperture_size}_kpc"
+    )
+    halo_mass = catalogue.get_quantity("masses.mass_200crit")
 
     smhm = stellar_mass / halo_mass
     name = f"$M_* / M_{{\\rm 200crit}}$ ({aperture_size} kpc)"
@@ -133,70 +140,42 @@ for aperture_size in aperture_sizes:
 
 # Now baryon fractions
 
-try:
-    Omega_m = catalogue.units.cosmology.Om0
-    Omega_b = catalogue.units.cosmology.Ob0
+Omega_m = catalogue.units.cosmology.Om0
+Omega_b = catalogue.units.cosmology.Ob0
 
-    M_500 = catalogue.spherical_overdensities.mass_500_rhocrit
-    M_500_gas = catalogue.spherical_overdensities.mass_gas_500_rhocrit
-    M_500_star = catalogue.spherical_overdensities.mass_star_500_rhocrit
-    M_500_baryon = M_500_gas + M_500_star
+M_500 = catalogue.get_quantity("spherical_overdensities.mass_500_rhocrit")
+M_500_gas = catalogue.get_quantity("spherical_overdensities.mass_gas_500_rhocrit")
+M_500_star = catalogue.get_quantity("spherical_overdensities.mass_star_500_rhocrit")
+M_500_baryon = M_500_gas + M_500_star
 
-    f_b_500 = (M_500_baryon / M_500) / (Omega_b / Omega_m)
-    name = "$f_{\\rm b, 500, true} / (\\Omega_{\\rm b} / \\Omega_{\\rm m})$"
-    f_b_500.name = name
+f_b_500 = (M_500_baryon / M_500) / (Omega_b / Omega_m)
+name = "$f_{\\rm b, 500, true} / (\\Omega_{\\rm b} / \\Omega_{\\rm m})$"
+f_b_500.name = name
 
-    f_gas_500 = (M_500_gas / M_500) / (Omega_b / Omega_m)
-    name = "$f_{\\rm gas, 500, true} / (\\Omega_{\\rm b} / \\Omega_{\\rm m})$"
-    f_gas_500.name = name
+f_gas_500 = (M_500_gas / M_500) / (Omega_b / Omega_m)
+name = "$f_{\\rm gas, 500, true} / (\\Omega_{\\rm b} / \\Omega_{\\rm m})$"
+f_gas_500.name = name
 
-    f_star_500 = (M_500_star / M_500) / (Omega_b / Omega_m)
-    name = "$f_{\\rm star, 500, true} / (\\Omega_{\\rm b} / \\Omega_{\\rm m})$"
-    f_star_500.name = name
+f_star_500 = (M_500_star / M_500) / (Omega_b / Omega_m)
+name = "$f_{\\rm star, 500, true} / (\\Omega_{\\rm b} / \\Omega_{\\rm m})$"
+f_star_500.name = name
 
-    setattr(self, "baryon_fraction_true_R500", f_b_500)
-    setattr(self, "gas_fraction_true_R500", f_gas_500)
-    setattr(self, "star_fraction_true_R500", f_star_500)
-except AttributeError:
-    # We did not produce these quantities, let's make an array of ones.
-    ones = unyt.unyt_array(
-        np.ones(np.size(catalogue.masses.mass_200crit)), "dimensionless"
-    )
-    setattr(
-        self,
-        "baryon_fraction_true_R500",
-        unyt.unyt_array(
-            ones,
-            name="$f_{\\rm b, 500, true} / (\\Omega_{\\rm b} / \\Omega_{\\rm m})$ not found, showing $1$",
-        ),
-    )
-    setattr(
-        self,
-        "gas_fraction_true_R500",
-        unyt.unyt_array(
-            ones,
-            name="$f_{\\rm gas, 500, true} / (\\Omega_{\\rm b} / \\Omega_{\\rm m})$ not found, showing $1$",
-        ),
-    )
-    setattr(
-        self,
-        "star_fraction_true_R500",
-        unyt.unyt_array(
-            ones,
-            name="$f_{\\rm star, 500, true} / (\\Omega_{\\rm b} / \\Omega_{\\rm m})$ not found, showing $1$",
-        ),
-    )
+setattr(self, "baryon_fraction_true_R500", f_b_500)
+setattr(self, "gas_fraction_true_R500", f_gas_500)
+setattr(self, "star_fraction_true_R500", f_star_500)
 
 for aperture_size in aperture_sizes:
-    stellar_mass = getattr(catalogue.apertures, f"mass_star_{aperture_size}_kpc")
+    stellar_mass = catalogue.get_quantity(
+        f"apertures.mass_star_{aperture_size}_kpc"
+    )
 
-    halo_M200crit = catalogue.masses.mass_200crit
+    halo_mass = catalogue.get_quantity("masses.mass_200crit")
     smhm = stellar_mass / halo_mass
     name = f"$M_* / M_{{\\rm 200crit}}$ ({aperture_size} kpc)"
     smhm.name = name
     setattr(self, f"stellar_mass_to_halo_mass_200crit_{aperture_size}_kpc", smhm)
 
-    halo_MBN98 = catalogue.masses.mass_bn98
+    halo_MBN98 = catalogue.get_quantity("masses.mass_bn98")
     smhm = stellar_mass / halo_MBN98
     name = f"$M_* / M_{{\\rm BN98}}$ ({aperture_size} kpc)"
     smhm.name = name
@@ -210,22 +189,15 @@ def register_star_magnitudes(self, catalogue, aperture_sizes):
     # Loop over apertures
     for aperture_size in aperture_sizes:
         for band in bands:
-            try:
-
-                L_AB = getattr(
-                    catalogue.stellar_luminosities,
-                    f"{band}_luminosity_{aperture_size}_kpc",
-                )
-                m_AB = np.copy(L_AB)
-                mask = L_AB > 0.0
-                m_AB[mask] = -2.5 * np.log10(m_AB[mask])
-                m_AB = unyt.unyt_array(m_AB, units="dimensionless")
-                m_AB.name = f"{band}-band AB magnitudes ({aperture_size} kpc)"
-                setattr(self, f"magnitudes_{band}_band_{aperture_size}_kpc", m_AB)
-
-            except AttributeError:
-                pass
-
+            L_AB = catalogue.get_quantity(
+                f"stellar_luminosities.{band}_luminosity_{aperture_size}_kpc"
+            )
+            m_AB = np.copy(L_AB)
+            mask = L_AB > 0.0
+            m_AB[mask] = -2.5 * np.log10(m_AB[mask])
+            m_AB = unyt.unyt_array(m_AB, units="dimensionless")
+            m_AB.name = f"{band}-band AB magnitudes ({aperture_size} kpc)"
+            setattr(self, f"magnitudes_{band}_band_{aperture_size}_kpc", m_AB)
     return
 
 
@@ -234,7 +206,9 @@ register_star_magnitudes(self, catalogue, aperture_sizes)
 # Add eddington bias to stellar masses, according to Behroozi (2019)
 
 for aperture_size in aperture_sizes:
-    stellar_mass = getattr(catalogue.apertures, f"mass_star_{aperture_size}_kpc")
+    stellar_mass = catalogue.get_quantity(
+        f"apertures.mass_star_{aperture_size}_kpc"
+    )
     bias_std = np.min(np.array([0.07 + 0.071 * catalogue.z, 0.3]))
     bias_factors = 10 ** (np.random.normal(0, bias_std, len(stellar_mass)))
 

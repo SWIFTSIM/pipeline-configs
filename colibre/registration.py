639ad4a55a8f28a4d9c155e6df06a2952e83de06
robjmcgibbon <robjmcgibbon@gmail.com>
2026-06-08 17:55:22 +0200

    Add HaloMassHIMass data

--- colibre/registration.py
+++ colibre/registration.py
@@ -658,6 +658,18 @@ def register_stellar_mass_scatter(scatter_amplitude):
     sphere.stellar_mass_with_scatter = scattered
 
 
+def register_host_halo_mass_bn98():
+    bn98_mass = soap.spherical_overdensity_bn98.total_mass.to_physical_value('Msun')
+    host_halo_index = soap.soap.host_halo_index.value
+
+    host_mass = unyt.unyt_array(bn98_mass, units='Msun')
+
+    is_satellite = soap.input_halos.is_central.value == 0
+    host_mass[is_satellite] = bn98_mass[host_halo_index[is_satellite]]
+
+    host_mass.name = "$M_{\\rm BN98}$ host halo"
+    soap.spherical_overdensity_bn98.host_halo_mass = host_mass
+
 register_sfr_derived(aperture_sizes)
 register_stellar_metallicities(aperture_sizes, solar_metal_mass_fraction)
 register_stellar_to_halo_mass_ratios(aperture_sizes)
@@ -678,3 +690,4 @@ register_stellar_magnitudes(aperture_sizes, BAND_COLUMNS)
 register_snia_rates_per_stellar_mass(aperture_sizes)
 register_stellar_mass_selection_masks(aperture_sizes)
 register_stellar_mass_scatter(stellar_mass_scatter_amplitude)
+register_host_halo_mass_bn98()

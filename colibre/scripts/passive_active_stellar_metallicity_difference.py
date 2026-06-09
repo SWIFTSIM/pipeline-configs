Squash units bug in passive active (#313)

colibre/scripts/passive_active_stellar_metallicity_difference.py
@@ -42,12 +42,12 @@ for filename, name in zip(catalogue_filenames, arguments.name_list):
 
     # Load quantities, ignoring objects below mass bin
     sphere = getattr(catalogue, f"exclusive_sphere_{aperture_size}kpc")
-    star_mass = sphere.stellar_mass
-    mask = star_mass > mass_bounds[0] * unyt.Msun
+    star_mass = sphere.stellar_mass.to_physical_value('Msun')
+    mask = star_mass > mass_bounds[0]
     star_mass = star_mass[mask]
     Fe_over_H = sphere.linear_mass_weighted_iron_over_hydrogen_of_stars[mask]
     sfr = sphere.star_formation_rate.to("Msun/yr")[mask]
-    is_central = catalogue.input_halos.is_central.astype(bool)[mask]
+    is_central = (catalogue.input_halos.is_central.value == 1)[mask]
     Fe_abundance = np.log10(Fe_over_H / solar_fe_abundance)
 
     # Calculate if galaxy is passive or star-forming using definition from Lyu+23